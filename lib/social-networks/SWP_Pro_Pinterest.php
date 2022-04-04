48ba55fb7736ab524a86e7f43eebca3b10ae7dc9
Greg Asatryan <goqflash@gmail.com>
2022-04-04 21:17:31 +0400

    PHP 8: Required parameter $alt follows optional parameter $size

--- lib/social-networks/SWP_Pro_Pinterest.php
+++ lib/social-networks/SWP_Pro_Pinterest.php
@@ -355,7 +355,7 @@ class SWP_Pro_Pinterest {
 	 * @return $html Our version of the markup.
 	 *
 	 */
-	public function classic_editor_add_pin_description( $html, $image_id, $caption, $title, $alignment, $url, $size = "", $alt ) {
+	public function classic_editor_add_pin_description( $html, $image_id, $caption, $title, $alignment, $url, $size = "", $alt = "" ) {
 		$pinterest_description = get_post_meta( $image_id, 'swp_pinterest_description', true );
 
 		if ( empty( $pinterest_description ) ) {
